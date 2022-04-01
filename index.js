80832e31baf4439813f2bf48d598fdf340f0ffd6
Jose Peña <josesamuelrd@gmail.com>
2022-04-01 10:03:11 -0300

    Code refactoring and added pagination

--- index.js
+++ index.js
@@ -1,70 +1,98 @@
-let pokemon_count = 0;
-let maxPokemons = 18;
-let page = 1;
-let main = document.querySelector(".main");
-let pokemonsWrapper = document.querySelector("#wrapper");
+const main = document.querySelector(".main");
+const pokemonsWrapper = document.querySelector(".wrapper");
+let pokemonCount = 0;
+let maxPokemons = 24;
+let currentPage = 1;
+function paginationBar() {
+  pokemonsWrapper.innerHTML += `                              
+        <ul class="pages">
+          <li class="page__prev btn__move" id="prev"><</li>
+          <li class="page__number" id="1">1</li>
+          <li class="page__number" id="2">2</li>
+          <li class="page__next btn__move" id="next">></li>
+        </ul>
+          `;
+
+  const pagePrev = document.querySelector(".page__prev");
+  const pageNext = document.querySelector(".page__next");
+  const pageNumber = document.querySelectorAll(".page__number");
+  pagePrev.addEventListener("click", () => {
+    if (currentPage > 1) {
+      pokemonsWrapper.innerHTML = "";
+      --currentPage;
+      pokemonCount = 18 * currentPage - 18;
+      getPokemon(++pokemonCount);
+    }
+  });
+  pageNext.addEventListener("click", () => {
+    pokemonsWrapper.innerHTML = "";
+    ++currentPage;
+    getPokemon(++pokemonCount);
+  });
+  for (let pageN of pageNumber) {
+    pageN.addEventListener("click", function () {
+      pokemonsWrapper.innerHTML = "";
+      currentPage = Number(pageN.innerText);
+      pokemonCount = 18 * currentPage - 18;
+      getPokemon(++pokemonCount);
+    });
+  }
+}
+function showPokemon(res, id) {
+  let stats = res.stats;
+  let [hp, att, def, spAtt, spDef, spd] = [...stats];
+  let [url, name] = [res.sprites["front_default"], res.name];
+  let types = res.types;
+  let createPokemon = document.createElement("div");
+  createPokemon.className = "pokemon";
+  pokemonsWrapper.appendChild(createPokemon);
+  createPokemon.innerHTML = `
+    <img src="${url}" alt="" class="pokemon__image"/>
+        <div class="pokemon__info">
+            <h2 class="pokemon__title">${name}</h2>
+            <ul class="pokemon__details">
+                <li class="details__stats">HP: <span class="value">${hp.base_stat}</span></li>
+                <li class="details__stats">Attack: <span class="value">${att.base_stat}</span></li>
+                <li class="details__stats">Defense: <span class="value">${def.base_stat}</span></li>
+                <li class="details__stats">Special-Attack: <span class="value">${spAtt.base_stat}</span></li>
+                <li class="details__stats">Special-Defense: <span class="value">${spDef.base_stat}</span></li>
+                <li class="details__stats">Speed: <span class="value">${spd.base_stat}</span></li>
+            </ul>
+            <ul class="pokemon__types" id="i${id}"></ul>
+      </div>`;
+  let pokemonTypes = document.querySelector(`#i${id}`);
+  for (let typePokemon of types) {
+    let newType = document.createElement("li");
+    newType.className = `types ${typePokemon.type.name}`;
+    newType.innerText = typePokemon.type.name;
+    pokemonTypes.appendChild(newType);
+  }
+}
+
 function getPokemon(id, index = 0) {
   let httpRequest = new XMLHttpRequest();
+  httpRequest.open("get", `https://pokeapi.co/api/v2/pokemon/${id}/`, true);
+  httpRequest.send();
   httpRequest.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let res = JSON.parse(this.response);
-      let stats = res.stats;
-      let [hp, att, def, spAtt, spDef, spd] = [...stats];
-      [hp, att, def, spAtt, spDef, spd] = [
-        hp.base_stat,
-        att.base_stat,
-        def.base_stat,
-        spAtt.base_stat,
-        spDef.base_stat,
-        spd.base_stat,
-      ];
-      let [url, name] = [res.sprites["front_default"], res.name];
-      let types = res.types;
-      let newPokemon = document.createElement("div");
-      newPokemon.className = "pokemon";
-      pokemonsWrapper.appendChild(newPokemon);
-      newPokemon.innerHTML = `<img
-                                  src="${url}"
-                                  alt=""
-                                  class="pokemon__image"
-                                />
-                                <div class="pokemon__info">
-                                  <h2 class="pokemon__title">${name}</h2>
-                                  <ul class="pokemon__details">
-                                    <li class="details__stats">HP: <span class="value">${hp}</span></li>
-                                    <li class="details__stats">Attack: <span class="value">${att}</span></li>
-                                    <li class="details__stats">Defense: <span class="value">${def}</span></li>
-                                    <li class="details__stats">Special-Attack: <span class="value">${spAtt}</span></li>
-                                    <li class="details__stats">Special-Defense: <span class="value">${spDef}</span></li>
-                                    <li class="details__stats">Speed: <span class="value">${spd}</span></li>
-                                  </ul>
-                                  <ul class="pokemon__types" id="i${id}">
-                                  </ul>
-                                </div>
-                          `;
-      console.log(pokemonsWrapper);
-
-      let pokemonTypes = document.querySelector(`#i${id}`);
-      for (el of types) {
-        let newType = document.createElement("li");
-        newType.className = `types ${el.type.name}`;
-        newType.innerText = el.type.name;
-        pokemonTypes.appendChild(newType);
-      }
-      if (pokemon_count >= page * maxPokemons) {
-        return 0;
+      showPokemon(res, id);
+      if (pokemonCount < currentPage * maxPokemons) {
+        getPokemon(++pokemonCount, ++index);
       } else {
-        ++index;
-        getPokemon(++pokemon_count,index);
+        paginationBar();
+        if (!document.getElementById(`${currentPage}`)) {
+          for (let i = 3; i <= currentPage; i++) {
+            let newPage = document.createElement("li");
+            newPage.className = "page__number";
+            newPage.id = i;
+            newPage.innerText = i;
+            document.querySelector(".pages").appendChild(newPage);
+          }
+        }
+        document.getElementById(`${currentPage}`).classList.add("current");
       }
     }
   };
-  httpRequest.open("get", `https://pokeapi.co/api/v2/pokemon/${id}/`, true);
-  httpRequest.send();
 }
-getPokemon(++pokemon_count);
-pokemonsWrapper.innerHTML += `                              <ul class="pages">
-<li class="page__prev btn__move" id="prev"><</li>
-<li class="page__number" id="current">1</li>
-<li class="page__number" id="2">2</li>
-<li class="page__next btn__move" id="next">></li>`;
+getPokemon(++pokemonCount);
